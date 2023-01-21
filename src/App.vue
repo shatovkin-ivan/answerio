f7e3bc30d524864ba8d0bfcaee57a72ec8e6297b
Shatovkin Ivan <shatowkin.ivan@yandex.ru>
2023-01-21 20:14:53 +0300

    add default styles

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 import HeaderComponent from './components/Header.vue'
 import AppAsk from './components/AppAsk.vue';
 import FooterComponent from './components/Footer.vue'
+import '@/assets/styles/style.scss'
 
 export default {
   name: 'App',
